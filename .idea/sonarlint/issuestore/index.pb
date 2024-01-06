cff68a4b851e7da9e6cacc4d95e63a91c73afc26
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2024-01-06 23:10:45 +0300

    feat: structure updates

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,9 @@
 main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
 >
 pyproject.toml,5/d/5d07e7d72637aa0d59c89d381fe6dc4cf46e2491
+w
+Gjupyter/screen/.ipynb_checkpoints/framebufferstate_new-checkpoint.ipynb,1/6/1624fdc7053f564db49e51820c551de8a979c1fc
+s
+Cjupyter/screen/.ipynb_checkpoints/framebufferstate-checkpoint.ipynb,d/b/dbf5956b181ea53ff076047b422d8b0cd1b5a17e
+U
+%jupyter/screen/framebufferstate.ipynb,3/d/3daf65598a0dd556029f9cb85e9afda8dea75ad2
--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-if __name__ == "__main__":
+import click
+
+
+@click.group()
+def server_cli():
     pass
+
+
+@click.group()
+def client_cli():
+    pass
+
+
+@server_cli.command()
+@click.option("--server-name", help="Server Display Name")
+@click.pass_context
+def server(ctx, server_name):
+    # by means other than the `if` block below)
+    click.echo(ctx)
+    click.echo("server")
+
+
+@client_cli.command()
+@click.option("--server-uuid", help="Client UUID")
+def client(server_uuid: str):
+    click.echo(f"client {server_uuid}")
+
+
+cli = click.CommandCollection(sources=[server_cli, client_cli])
+
+if __name__ == "__main__":
+    cli()
